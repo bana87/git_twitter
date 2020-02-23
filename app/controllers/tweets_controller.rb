50839e358cbda77629d3ce127e75ef2e40f51d98
EC2 Default User <ec2-user@ip-172-31-45-144.ap-northeast-1.compute.internal>
2020-02-23 15:05:26 +0000

    データの保存まで, 最低限のcss

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,18 @@
 class TweetsController < ApplicationController
   def index
+    @tweets = Tweet.all
+  end
+  
+  def new
+    @tweet = Tweet.new #たぶんなくても良い
+  end
+  
+  def create
+    Tweet.create(tweet_params)
+  end
+  
+  private
+  def tweet_params
+    params.permit(:name, :text)
   end
 end
