329cdbb687fc9ed7b20be584daf223c57c3e9acc
EC2 Default User <ec2-user@ip-172-31-45-144.ap-northeast-1.compute.internal>
2020-02-24 08:56:49 +0000

    Tweetテーブルのnameカラムを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_24_083814) do
+ActiveRecord::Schema.define(version: 2020_02_24_085426) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
