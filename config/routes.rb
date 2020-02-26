b8cd7f6ee902e4d002c148c206f7a6683f4d472e
EC2 Default User <ec2-user@ip-172-31-45-144.ap-northeast-1.compute.internal>
2020-02-26 11:26:12 +0000

    routesにuserのupdateを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root :to => 'tweets#index'
-  get     '/tweets'     => 'tweets#index'
-  post    '/tweets'     => 'tweets#create'
-  delete  '/tweets/:id' => 'tweets#destroy'
-  get     'users/:id'   => 'users#show'
+  get     '/tweets'       => 'tweets#index'
+  post    '/tweets'       => 'tweets#create'
+  delete  '/tweets/:id'   => 'tweets#destroy'
+  get     'users/:id'     => 'users#show'
+  get     'users/:id/edit'=> 'users#update'
 end
