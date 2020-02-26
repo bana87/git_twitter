ddb36d1a6bdc808c55d182be04b23c8cb14cd104
EC2 Default User <ec2-user@ip-172-31-45-144.ap-northeast-1.compute.internal>
2020-02-26 14:18:47 +0000

    ユーザー編集機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users,
-    controllers:{registrations:'registrations'}
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations'
+  }
   root :to => 'tweets#index'
   get     '/tweets'       => 'tweets#index'
   post    '/tweets'       => 'tweets#create'
