35453b22fd64d49811838c5a86238395af6d55fe
EC2 Default User <ec2-user@ip-172-31-45-144.ap-northeast-1.compute.internal>
2020-02-29 08:52:05 +0000

    fav controllerにcreateとdestroyを定義, indexにfavcountを追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,15 @@
 class FavoritesController < ApplicationController
+  def create
+    @fav = Favorite.new(user_id: current_user.id, tweet_id: params[:tweet_id])
+    @fav.save
+    @tweet = Tweet.find_by(id: @fav.tweet_id)
+    @fav_count = Favorite.where(tweet_id: params[:tweet_id]).count
+  end
+  
+  def delete
+    @fav = Favorite.new(user_id: current_user.id, tweet_id: params[:tweet_id])
+    @tweet = Tweet.find_by(id: @fav.tweet_id)
+    @fav.destroy
+    @fav_count = Favorite.where(tweet_id: params[:tweet_id]).count
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => {
     :registrations => 'users/registrations'
   }
-  root :to => 'tweets#index'
-  get     '/tweets'       => 'tweets#index'
-  post    '/tweets'       => 'tweets#create'
-  delete  '/tweets/:id'   => 'tweets#destroy'
-  get     'users/:id'     => 'users#show'
+  root :to  => 'tweets#index'
+  get     '/tweets', to: 'tweets#index'
+  post    '/tweets', to: 'tweets#create'
+  delete  '/tweets/:id', to: 'tweets#destroy'
+  get     'users/:id', to: 'users#show'
+  post "favorites/:tweet_id/create", to: "favorites#create", constraints: {tweet_id: /\d+/}, as: :favorites_create
+  post "favorites/:tweet_id/delete", to: "favorites#delete", constraints: {tweet_id: /\d+/}, as: :favorites_delete
 end
