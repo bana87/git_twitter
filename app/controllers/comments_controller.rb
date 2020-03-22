f0ff668916a965fc496354ded2793a097325ab69
EC2 Default User <ec2-user@ip-172-31-45-144.ap-northeast-1.compute.internal>
2020-03-22 13:23:09 +0000

    css微調整

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,10 @@ class CommentsController < ApplicationController
     comment = Comment.new(text: comment_params[:text], user_id: current_user.id, tweet_id: @tweet.id)
     if comment.save
       flash[:notice] = 'コメントが完了したよ'
+      redirect_back(fallback_location: root_path)
     else
       flash[:alert] = 'コメントに失敗しました。もう一度コメントしてみましょう。'
+      redirect_back(fallback_location: root_path)
     end
   end
   
