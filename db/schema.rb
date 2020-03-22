be09e0ba16fd6bd2635e77be70a14ff8800488ec
EC2 Default User <ec2-user@ip-172-31-45-144.ap-northeast-1.compute.internal>
2020-03-22 09:53:51 +0000

    commentモデル, テーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_131448) do
+ActiveRecord::Schema.define(version: 2020_03_22_095036) do
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
