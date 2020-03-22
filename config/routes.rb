ea106a48daade035374b150f493005dae3862e46
EC2 Default User <ec2-user@ip-172-31-45-144.ap-northeast-1.compute.internal>
2020-03-22 11:25:33 +0000

    tweetのshowcontroller, viewを作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,14 @@ Rails.application.routes.draw do
     :registrations => 'users/registrations'
   }
   root :to  => 'tweets#index'
-  get     '/tweets', to: 'tweets#index'
-  post    '/tweets', to: 'tweets#create'
-  delete  '/tweets/:id', to: 'tweets#destroy'
-  get     'users/:id', to: 'users#show'
+  resources :tweets
+  resources :users, only: [:show]
   post   '/like/:tweet_id' => 'likes#like',   as: 'like'
   delete '/like/:tweet_id' => 'likes#unlike', as: 'unlike'
+ 
+  #get     '/tweets', to: 'tweets#index'
+  #post    '/tweets', to: 'tweets#create'
+  #delete  '/tweets/:id', to: 'tweets#destroy'
+  #get     'users/:id', to: 'users#show'
+  
 end
